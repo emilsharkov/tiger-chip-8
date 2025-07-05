28c56a3c5ad5318234c89706c55e76b31f64de24
Emil Sharkov <emosharkov@gmail.com>
2025-07-05 00:58:03 -0700

    wasm again

---
+++ web/src/app/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useState } from 'react';
+import { useChip8 } from '@/hooks/useChip8';
+
+export default function Home() {
+  const [romBytes, setRomBytes] = useState<Uint8Array | null>(null);
+
+  useChip8(romBytes);
+
+  const handleRomUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+    const buffer = await file.arrayBuffer();
+    setRomBytes(new Uint8Array(buffer));
+  };
+
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold mb-4">CHIP-8 Emulator</h1>
+      <canvas id="canvas"/>
+      <audio id="audio"/>
+      <input type="file" onChange={handleRomUpload} />
+    </main>
+  );
+}
+
---
+++ web/src/hooks/useChip8.tsx
@@ -0,0 +1,57 @@
+import { get_audio, get_canvas_context, get_height, get_width, WasmEmulator } from "@/wasm/tiger_chip_8_wasm";
+import { useEffect } from "react";
+
+const useChip8 = (romBytes: Uint8Array | null) => {
+    useEffect(() => {
+        if (!romBytes) return;
+
+        let emulator: WasmEmulator;
+        let animationFrameId: number;
+        const keydownHandler = (event: KeyboardEvent) => {
+            const keycode = emulator?.to_keycode(event.key);
+            if (keycode !== undefined) {
+                emulator?.handle_key_press(keycode, true);
+            }
+        };
+        const keyupHandler = (event: KeyboardEvent) => {
+            const keycode = emulator?.to_keycode(event.key);
+            if (keycode !== undefined) {
+                emulator?.handle_key_press(keycode, false);
+            }
+        };
+
+        const width = get_width();
+        const height = get_height();
+        const scale = 10;
+        const opPerFrame = 10;
+
+        const ctx = get_canvas_context(width, height, scale);
+        const audio = get_audio();
+
+        emulator = new WasmEmulator(ctx, audio);
+        emulator.load_font_set();
+        emulator.load_rom(romBytes);
+
+        document.addEventListener('keydown', keydownHandler);
+        document.addEventListener('keyup', keyupHandler);
+
+        const renderFrame = () => {
+            for (let i = 0; i < opPerFrame; i++) {
+                emulator.emulate_instruction();
+            }
+            emulator.tick_timers();
+            emulator.draw_screen(width, scale);
+            animationFrameId = requestAnimationFrame(renderFrame);
+        };
+
+        renderFrame();
+
+        return () => {
+            document.removeEventListener('keydown', keydownHandler);
+            document.removeEventListener('keyup', keyupHandler);
+            cancelAnimationFrame(animationFrameId);
+        };
+    }, [romBytes]);
+}
+
+export { useChip8 };
